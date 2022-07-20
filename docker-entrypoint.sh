fea24ba2ecfc543c1ca0a462386f17e56338baa0
Bastien Tagliaro <bastien.tagliaro@inria.fr>
2022-07-20 16:33:57 +0200

    Remove default single server configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -u -e -o pipefail
 
-upshost="${UPS_HOST:-127.0.0.1}"
-upsport="${UPS_PORT:-3493}"
-upsuser="${UPS_USER:-monuser}"
-upspassword="${UPS_PASSWORD:-secret}"
-
-echo "server = '$upshost'" > /app/webNUT/webnut/config.py
-echo "port = '$upsport'" >> /app/webNUT/webnut/config.py
-echo "username = '$upsuser'" >> /app/webNUT/webnut/config.py
-echo "password = '$upspassword'" >> /app/webNUT/webnut/config.py
-
-cat /app/webNUT/webnut/config.py
 cd /app/webNUT && python setup.py install
 
 cd webnut
